bcb87fe042fc06df91d6b3b06ce9fe54e55817cd
santiagocata <93060684+santiagocata@users.noreply.github.com>
2022-03-28 11:46:52 -0300

    Update and rename Grid.js to Grid.jsx

--- src/commons/Grid.jsx
+++ src/commons/Grid.jsx
@@ -3,7 +3,7 @@ import { experimentalStyled as styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import ResponsiveCard from "./Card.js";
+import ResponsiveCard from "./Card";
 
 export default function ResponsiveGrid() {
   return (
