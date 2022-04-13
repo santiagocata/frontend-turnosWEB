53f9be22400278e9adb35c3cd378fec1c07c55ed
Javier Albertoni <57158052+Habbi2@users.noreply.github.com>
2022-04-13 16:25:11 -0300

    Update AdminView.jsx

--- src/components/AdminView.jsx
+++ src/components/AdminView.jsx
@@ -212,7 +212,6 @@ const AdminView = ({ type, setBranchs, setVisibility, selectedBranch }) => {
       form.coords = setCoords();
       handleEmptyValue(form);
       handleEmptyValue(state.location);
-      console.log(handleEmptyValue(state.form));
       if (handleEmptyValue(state.form) && handleEmptyValue(state.location)) {
         await axios.post("/branch/register", form);
         const branchs = await axios.get("/branch/adminview");
